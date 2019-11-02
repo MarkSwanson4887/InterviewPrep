76002fb1cdc5bfd65dc0fd9560ed5d1c7df2f477
MarkSwanson4887 <matthew.swentzel@gmail.com>
2019-11-02 13:58:58 -0400

    Completed lengthOfLastWord

--- LeetCodeSolutions/Easy/lengthOfLastWord.java
+++ LeetCodeSolutions/Easy/lengthOfLastWord.java
@@ -1,23 +1,35 @@
 class lengthOfLastWord {
-     public int lengthOfLastWord(String s) {
-        boolean spaceConfirmation = false; int lastWord = 0;
+     public static int lengthOfLastWord(String s) {
+        int lastWord = 0;
         char[] stringArray = s.toCharArray();
-        
-        for(int i = 0; i < stringArray.length; i++){
-            if(Character.isLetter(stringArray[i]) == false){
-                spaceConfirmation = true;
+        boolean endNull = false;
+
+        if(s.isEmpty() == true){
+            return 0;
+        }
+        if(Character.isLetter(stringArray[s.length() - 1]) == false){
+            endNull = true;
+        }
+        for(int i = stringArray.length - 1;i >= 0;i--){
+            if(endNull == true){
+                if(Character.isLetter(stringArray[i]) == true){
+                    endNull = false;
+                }
             }
-            if(spaceConfirmation == true){
+            if(endNull == false){
+                if(Character.isLetter(stringArray[i]) == false){
+                    break;
+                }
                 lastWord++;
-            }
-            if(spaceConfirmation == true && Character.isLetter(stringArray[i]) == false){
-                System.out.println("here");
-                lastWord = 0;
-                spaceConfirmation = false;
             }
         }
         return lastWord;
     }
     public static void main(String args[]){
+        System.out.println(lengthOfLastWord("Hello World") + ": The result of this should be 5");
+        System.out.println(lengthOfLastWord("Hi my name is matthew swentzel") + ": The result of this should be 8");
+        System.out.println(lengthOfLastWord(" ") + ": The result of this should be 0");
+        System.out.println(lengthOfLastWord("aaa  ") + ": The result of this should be 3");
+        System.out.println(lengthOfLastWord("a a ") + ": The result of this should be 1");
     }
 }
