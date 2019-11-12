a30395e46285037954891a3ce3292533f2fae6fc
MarkSwanson4887 <matthew.swentzel@gmail.com>
2019-11-12 02:10:58 -0500

    Finished isSametree

--- LeetCodeSolutions/Easy/isSameTree.java
+++ LeetCodeSolutions/Easy/isSameTree.java
@@ -1,12 +1,25 @@
 //Unfinished 
+class isSameTree{
 
-public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null){
-            return true;
-            }
-        if(p != null || q != null){
-            return false;
-        }
-        
-        return (p == q) && isSameTree(p.left,q.left) && ; 
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
     }
+
+    public static boolean isSameTreeS(TreeNode p, TreeNode q) {
+            if(p == null && q == null){
+                return true;
+            }
+            if((p != null && q == null) || (p == null && q != null)){
+                return false;
+            }
+            else{
+                return (p.val == q.val) && isSameTreeS(p.left,q.left) && isSameTreeS(p.right,q.right);
+            }
+        }
+        public static void main(String[] args){
+            System.out.println("Solution passed all leetcode tests");
+        }
+}
