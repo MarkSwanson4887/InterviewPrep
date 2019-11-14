b90225aadbf03011c4b7c5b0137fe6aa32259db4
MarkSwanson4887 <matthew.swentzel@gmail.com>
2019-11-13 23:10:15 -0500

    Started is symTree

---
+++ LeetCodeSolutions/Easy/symTree.java
@@ -0,0 +1,15 @@
+class symTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; 
+    }
+    public static boolean isSymmetric(TreeNode root){
+        
+    }
+
+    public static void main(String agrs[]){
+
+    }
+}
