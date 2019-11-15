6b4dcae0ec668d48ac9b11e1f15880da29e3e516
MarkSwanson4887 <matthew.swentzel@gmail.com>
2019-11-15 00:54:31 -0500

    completed symTree

--- LeetCodeSolutions/Easy/symTree.java
+++ LeetCodeSolutions/Easy/symTree.java
@@ -1,17 +1,25 @@
 class symTree {
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; 
-    }
-    public static boolean isSymmetric(TreeNode root){
-        if(root.right == null && root.left == null){
-            return true;
-        }
-    }
-
     public static void main(String agrs[]){
-
+        //Utilizes a helper function so the tree can be split into two seperate trees
+       class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode(int x) { val = x; 
+        }
+       boolean helper(TreeNode root1, TreeNode root2){
+            if(root1 == null && root2 == null){
+                return true;
+            }
+            if((root1 == null && root2 != null) || (root1 != null && root2 == null)){
+                return false;
+            }
+            return (root1.val == root2.val) && helper(root1.left,root2.right) && helper(root1.right,root2.left);
+        }
+        
+        boolean isSymmetric(TreeNode root) {
+            return helper(root,root);
+        }
+        }
     }
 }
