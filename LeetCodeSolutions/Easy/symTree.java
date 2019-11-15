0abc369e07a7f898559f8d8adfac5c91050834b9
MarkSwanson4887 <matthew.swentzel@gmail.com>
2019-11-14 23:11:06 -0500

    added base case to symTree

--- LeetCodeSolutions/Easy/symTree.java
+++ LeetCodeSolutions/Easy/symTree.java
@@ -6,7 +6,9 @@ class symTree {
         TreeNode(int x) { val = x; 
     }
     public static boolean isSymmetric(TreeNode root){
-        
+        if(root.right == null && root.left == null){
+            return true;
+        }
     }
 
     public static void main(String agrs[]){
