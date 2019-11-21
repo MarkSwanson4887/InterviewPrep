04b8c618943603f174d956c22b96f4101a209fcc
MarkSwanson4887 <matthew.swentzel@gmail.com>
2019-11-21 01:46:26 -0500

    solution to maximumDepth

--- LeetCodeSolutions/Easy/maximumDepth.java
+++ LeetCodeSolutions/Easy/maximumDepth.java
@@ -1,7 +1,21 @@
-class maximumDepth {    
-    public int helper(TreeNode root1, int count){ 
+class maximumDepth {
 
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){ 
+            val = x; 
+        }
     }
-    public int maxDepth(TreeNode root) {   
+
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+    public static void main(String args[]){
+        System.out.println("passed all tests on LeetCode");
     }
 }
